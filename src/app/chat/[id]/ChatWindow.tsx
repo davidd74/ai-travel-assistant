c7889949c7f6909bdfd4c76a334585088f71074c
davidd74 <lolercoolol@gmail.com>
2024-07-03 12:43:36 +0200

    refactored Chat window code and fixed height issue

--- src/app/chat/[id]/ChatWindow.tsx
+++ src/app/chat/[id]/ChatWindow.tsx
@@ -58,10 +58,6 @@ const ChatWindow = () => {
   };
 
   useEffect(() => {
-    textArea.current!.focus();
-  }, []);
-
-  useEffect(() => {
     if (chatMessage === "") {
       textArea.current!.style.height = "auto";
     }
@@ -76,113 +72,87 @@ const ChatWindow = () => {
   }, [conversation.length]);
 
   return (
-    <div className="w-full md:pt-4 md:pl-12 relative overflow-y-auto flex flex-col h-screen max-h-screen md:items-start md:ml-12 px-4 md:px-2 justify-between">
-      <nav className="flex gap-4 py-2.5 fixed shadow-sm md:hidden left-0 top-0 w-full justify-center bg-light-background border-b-2 items-center">
-        <button className="bg-light-box_bg rounded-lg p-1 shadow-sm border-2 hover:border-light-border transition-all duration-500 ease-out absolute left-4">
-          <HamburgerIcon />
-        </button>
-        <button
-          onClick={() => setView("chat")}
-          className="bg-light-box_bg border-2 shadow-sm hover:border-light-border transition-all duration-500 ease-out py-2 px-4 rounded-lg cursor-pointer"
-        >
-          AI Chat
-        </button>
-        <button
-          onClick={() => setView("itinerary")}
-          className="bg-light-box_bg border-2 shadow-sm hover:border-light-border transition-all duration-500 ease-out py-2 px-4 rounded-lg cursor-pointer"
-        >
-          Itinerary
-        </button>
+    <div className="w-full flex flex-col justify-between overflow-hidden">
+      <nav className="pl-4  h-[55px] bg-red-500 fixed w-full top-0 left-0 md:hidden">
+        nav here
       </nav>
-      <div className="h-full flex flex-col justify-between mb-[50px]">
-        {view === "chat" ? (
-          <>
-            <div className="md:ml-[20px] md:mt-0  chat-width chat-window mb-4">
-              <div className="mt-4 space-y-8">
-                <div className="chat-width mt-[80px] md:mt-0 flex gap-3">
-                  <Image
-                    width={32}
-                    height={32}
-                    src="/images/assistant-pfp.png"
-                    alt="assistant pfp"
-                    className="aspect-square w-[32px] h-[32px] border-1 border-gray-500 rounded-full"
-                  />
-                  <div className="flex flex-col">
-                    <Image
-                      src={"/images/tapestry.jpg"}
-                      width={600}
-                      height={600}
-                      alt="Cultural Tapestry of Spain."
-                      className="w-full lg:w-3/5 rounded-t-[8px]"
-                    />
-                    <p className="text-[#00526C] text-sm pt-3 pb-1">
-                      Aug 10 - Aug 17
-                    </p>
-                    <h4 className="text-xl font-semibold lg:w-3/5">
-                      Cultural Tapestry of Spain.
-                    </h4>
-                    <p className="text-[rgba(0,0,0,0.6)] text-sm font-medium w-11/12 lg:w-3/5 pr-2">
-                      Experience the Allure of Spain: Rich Culture, Stunning
-                      Landscapes, and More! Vibrant Culture and Exquisite
-                      Cuisine! Build your best memories in one of the magic
-                      places.
-                    </p>
-                    <button className="w-fit mt-4 px-4 py-2 rounded-md text-white bg-light-primary text-sm hover:bg-light-primary/80 transition-all duration-300 ease-linear">
-                      Book Now
-                    </button>
-                  </div>
-                </div>
-                {conversation.slice(1).map((message, index) => (
-                  <div key={index} className={`flex gap-3 items-center`}>
-                    {message.role === "assistant" ? (
-                      <Image
-                        width={32}
-                        height={32}
-                        src="/images/assistant-pfp.png"
-                        alt="assistant pfp"
-                        className="self-start aspect-square w-[32px] h-[32px] border-1 border-gray-500 rounded-full"
-                      />
-                    ) : (
-                      <Image
-                        width={32}
-                        height={32}
-                        src="/images/user-pfp.jpg"
-                        alt="assistant pfp"
-                        className="aspect-square w-[32px] h-[32px] border-1 border-gray-500 rounded-full"
-                      />
-                    )}
-                    <p className="w-full md:w-1/2">{message.content}</p>
-                  </div>
-                ))}
-              </div>
-              <div ref={div}></div>
+      <div className="pl-4 chat-height overflow-y-auto chat-scrollbar flex flex-col">
+        <div className="chat-width mt-[75px] pr-6 md:mt-4 flex gap-3">
+          <Image
+            width={32}
+            height={32}
+            src="/images/assistant-pfp.png"
+            alt="assistant pfp"
+            className="aspect-square w-[32px] h-[32px] border-1 border-gray-500 rounded-full"
+          />
+          <div className="flex flex-col">
+            <Image
+              src={"/images/tapestry.jpg"}
+              width={600}
+              height={600}
+              alt="Cultural Tapestry of Spain."
+              className="w-full lg:w-3/5 rounded-t-[8px]"
+            />
+            <p className="text-[#00526C] text-sm pt-3 pb-1">Aug 10 - Aug 17</p>
+            <h4 className="text-xl font-semibold lg:w-3/5">
+              Cultural Tapestry of Spain.
+            </h4>
+            <p className="text-[rgba(0,0,0,0.6)] text-sm font-medium w-11/12 lg:w-3/5 pr-2">
+              Experience the Allure of Spain: Rich Culture, Stunning Landscapes,
+              and More! Vibrant Culture and Exquisite Cuisine! Build your best
+              memories in one of the magic places.
+            </p>
+            <button className="w-fit mt-4 px-4 py-2 rounded-md text-white bg-light-primary text-sm hover:bg-light-primary/80 transition-all duration-300 ease-linear">
+              Book Now
+            </button>
+          </div>
+        </div>
+
+        <div className="space-y-6 mt-10">
+          {conversation.slice(1).map((message, index) => (
+            <div key={index} className={`flex gap-3 items-center`}>
+              {message.role === "assistant" ? (
+                <Image
+                  width={32}
+                  height={32}
+                  src="/images/assistant-pfp.png"
+                  alt="assistant pfp"
+                  className="self-start aspect-square w-[32px] h-[32px] border-1 border-gray-500 rounded-full"
+                />
+              ) : (
+                <Image
+                  width={32}
+                  height={32}
+                  src="/images/user-pfp.jpg"
+                  alt="assistant pfp"
+                  className="aspect-square w-[32px] h-[32px] border-1 border-gray-500 rounded-full"
+                />
+              )}
+              <p className="w-full md:w-1/2">{message.content}</p>
             </div>
-            <div className="chat-wrapper left-0 right-0 flex justify-center relative  bg-light-background">
-              <form className="px-4 md:px-0 w-full md:w-11/12 md:relative">
-                <textarea
-                  placeholder="Ask me anything..."
-                  rows={1}
-                  ref={textArea}
-                  onInput={inputHandler}
-                  value={chatMessage}
-                  onChange={(e) => setChatMessage(e.target.value)}
-                  className="caret-light-primary_darker py-4 rounded-[8px] pl-3 pr-14 w-full bg-[#F9F9F9] mt-4 outline-none font-medium chat-textarea resize-none border-[3px] border-light-border_lighter placeholder:text-[#808080] placeholder:font-normal"
-                  style={{ maxHeight: "150px" }}
-                ></textarea>
-                <button
-                  onClick={sendMessage}
-                  className="absolute right-[20px] top-1/2 translate-y-[-35%]"
-                >
-                  <Airplane2DIcon />
-                </button>
-              </form>
-            </div>
-          </>
-        ) : (
-          <>
-            <Itinerary display="block md:hidden" />
-          </>
-        )}
+          ))}
+          <div ref={div} />
+        </div>
+      </div>
+      <div className="flex justify-center fixed md:static w-full left-0 bottom-0">
+        <form className="px-4 md:px-0 w-full md:w-11/12 relative">
+          <textarea
+            placeholder="Ask me anything..."
+            rows={1}
+            ref={textArea}
+            onInput={inputHandler}
+            value={chatMessage}
+            onChange={(e) => setChatMessage(e.target.value)}
+            className=" caret-light-primary_darker py-4 rounded-[8px] pl-3 pr-14 w-full bg-[#F9F9F9] mt-4 outline-none font-medium chat-textarea resize-none border-[3px] border-light-border_lighter placeholder:text-[#808080] placeholder:font-normal"
+            style={{ maxHeight: "150px" }}
+          ></textarea>
+          <button
+            onClick={sendMessage}
+            className="absolute right-[20px] top-1/2 translate-y-[-35%]"
+          >
+            <Airplane2DIcon />
+          </button>
+        </form>
       </div>
     </div>
   );
